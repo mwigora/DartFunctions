c825fc030e248e3cd0dbe4b7856c5b690f0bf377
Horace <joelhorace2@gmail.com>
2024-03-03 21:38:47 +0300

    DartFunctions

---
+++ Dartfunctions.dart
@@ -0,0 +1,23 @@
+int addTwo(int a, int b) {
+  return a + b;
+}
+
+int subtractTwo(int a, int b) {
+  return a - b;
+}
+
+int multiplyTwo(int a, int b) {
+  return a * b;
+}
+
+double divideTwo(int a, int b) {
+  return a / b;
+}
+
+int stringLength(String text) {
+  return text.length;
+}
+
+dynamic getFirstElement(List list) {
+  return list[0];
+}
